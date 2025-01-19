228121daac66955a41fd9da26e64623a7ba96285
kavi castelo <kavindu123450@gmail.com>
2025-01-19 19:29:50 +0530

    assets not loading fix

--- src/app/components/forbidden/forbidden.component.ts
+++ src/app/components/forbidden/forbidden.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-forbidden',
   standalone: true,
   templateUrl: './forbidden.component.html',
+  imports: [
+    NgOptimizedImage
+  ],
   styleUrls: ['./forbidden.component.scss']
 })
 export class ForbiddenComponent {
