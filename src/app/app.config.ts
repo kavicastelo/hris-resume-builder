33007c750becb301165612487d1b1a9e3eeb514c
kavi castelo <kavindu123450@gmail.com>
2025-02-03 20:41:50 +0530

    frontend api endpoint

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {HttpClient, provideHttpClient} from '@angular/common/http';
+import {provideToastr} from 'ngx-toastr';
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideToastr({
+      timeOut: 5000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
+    provideAnimations()
   ]
 };
