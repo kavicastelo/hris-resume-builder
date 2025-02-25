85504a7c340888d27d944f097869e5dfba89fc92
kavi castelo <kavindu123450@gmail.com>
2025-02-25 18:08:21 +0530

    add missing utilities

---
+++ src/app/shared/utilities/utilities.ts
@@ -0,0 +1,80 @@
+export var Utilities = {
+  companyName: 'Talent Boozt',
+  contactNumber: '+94 74 410 4850',
+  contactNumber2: '+358 45 197 6700',
+  contactEmail: 'talentboozt@gmail.com',
+  contactLocation: 'Matara, Sri Lanka',
+  contactLocation2: 'Helsinki, Finland',
+  popularJobCategories: [
+    {
+      id: "1",
+      name: "Business Development",
+      icon: "fa fa-gitlab"
+    },
+    {
+      id: "2",
+      name: "Data Entry Operator",
+      icon: "fa fa-database"
+    },
+    {
+      id: "3",
+      name: "Graphic Designer",
+      icon: "fa fa-paint-brush"
+    },
+    {
+      id: "4",
+      name: "Web Developer",
+      icon: "fa fa-laptop"
+    }
+  ],
+  facebook: "https://www.facebook.com/people/Talent-Boozt/61568541891062/",
+  linkedin: "https://www.linkedin.com/company/talent-boozt/",
+  twitter: "https://x.com/TalentBoozt",
+  github: "https://www.github.com",
+  instagram: "https://www.instagram.com",
+  youtube: "https://www.youtube.com/@TalentBoozt",
+  faq: [
+    {
+      id: "1",
+      category: "General Questions",
+      questions: [
+        {
+          id: 1,
+          question: "What Makes Your Resume Service Unique?",
+          answer: "Our experts have over 12 years of experience\n" +
+            " in various industries and have in depth\n" +
+            " knowledge and hands on experience in interviewing candidates \n" +
+            "and recruiting them. Hence, once we get your request, we make \n" +
+            "a diligent effort to ensure that your CV matched what the job\n" +
+            " asks for. Our experts creates the best version of you and your \n" +
+            "story through your CV\n"
+        },
+        {
+          id: 2,
+          question: "How Long Does It Take to Receive My Final Resume?",
+          answer: "The standard time to receive your final Resume would be 4 to 6 \n" +
+            "business days"
+        },
+        {
+          id: 3,
+          question: "Can You Rush My Order?",
+          answer: "If you have an urgent requirement for your CV, you may contact us through whatspp after sending the request. We will do our best to attend your urgent request.  "
+        },
+        {
+          id: 4,
+          question: "How Many Revisions Can I Request?",
+          answer: "You can request up to 3 revisions within 2 days of receiving your first draft to \n" +
+            "ensure your CV meets your expectations. Our goal is to provide you with a high-quality, \n" +
+            "tailored CV that showcases your skills effectively.\n" +
+            " If you need further refinements beyond this,  additional revisions may be available at a small fee."
+        },
+        {
+          id: 5,
+          question: "What Formats Do You Provide",
+          answer: "We provide a standard PDF format that is ATS\n" +
+            " ( applicant tracking system) friendly. "
+        }
+      ]
+    }
+  ]
+}
